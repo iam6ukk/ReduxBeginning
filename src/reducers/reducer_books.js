6dbcfb4da4d11b4d227d02fab308a3498db91baa
eukk <eukk@fnsvalue.co.kr>
2021-07-29 17:43:58 +0900

    commit2

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function () {
-    return[
-        {title:'Javascript: The Good Parts'},
-        {title: 'Harry Potter'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'}
-
-    ]
+  return [
+    { title: "Javascript: The Good Parts" },
+    { title: "Harry Potter" },
+    { title: "The Dark Tower" },
+    { title: "Eloquent Ruby" },
+    // 책 배열 반환
+  ];
 }
